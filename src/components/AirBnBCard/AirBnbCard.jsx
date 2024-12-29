de5edcd1ece9b58ebe5932c50866be6b321aaaf1
Sri Konreddy <srikonreddy@Sris-MacBook-Air.local>
2024-12-29 17:46:37 +0000

    stlying on phone

--- src/components/AirBnBCard/AirBnbCard.jsx
+++ src/components/AirBnBCard/AirBnbCard.jsx
@@ -8,6 +8,7 @@ export const AirBnBCard = () => {
         <img src="https://a0.muscache.com/im/pictures/hosting/Hosting-774679902908916423/original/06668386-8944-42a9-8507-4d9bbbe89f1f.jpeg?sw_cache=trips_images&im_w=960&im_format=avif" alt="Birthday Celebration" className={cardStyles.image} />
         <div className={cardStyles.body}>
           <p><strong>Address: </strong>45 St. John's Rd, Boscombe, Bournemouth, BH5 1AQ, UK</p>
+          <p><strong>Parking: </strong>There is space for 2/3 cars to be parked on the driveway & on street parking in surrounding streets.</p>
         </div>
         <div onClick={() => window.open('https://air.tl/4VYiKAhz?s=67&unique_share_id=b8b1c3a9-fa27-4b50-90d6-780412cbeab2')} className={cardStyles.footer}>Click here to view the booking</div>
       </div>
--- src/components/ScheduleCard/ScheduleCard.jsx
+++ src/components/ScheduleCard/ScheduleCard.jsx
@@ -7,13 +7,17 @@ const ScheduleCard = () => {
       <div className="schedule-item">
         <h3>Friday Night (10th)</h3>
         <p>6:30 PM - Gather at the Airbnb</p>
-        <p>9:30 PM - Drinks in the town centre</p>
+        <p>9:30 PM - Drinks @ Aruba</p>
+        <img 
+          src="https://www.visitbournemouth.com/media/zoo/images/aruba_23fb7921605331036cab5119fb99a16b.jpg" 
+          alt="Drinks in the town centre" 
+          className="schedule-image"
+        />
       </div>
       <div className="schedule-item">
         <h3>Saturday Night (11th)</h3>
         <p>6:00 PM - My family arrives at the Airbnb</p>
         <p>7:00 PM - Birthday dinner</p>
-        <p>9:00 PM - Games and celebration</p>
       </div>
     </div>
   );
